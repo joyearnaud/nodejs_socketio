f3ef7fe63e2bd55b52fc3e774c3fdf492e7bc162
Arnaud J <joye.arnaud@gmail.com>
2019-03-21 12:12:56 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,12 @@ const morgan = require('morgan');
 const socketioapp = require("./socketIO/socketioapp");
 const apiapp = require("./API/apiapp");
 
-const corsOptions = { origin: 'https://meetus.netlify.com/', optionsSuccessStatus: 200}            
+const corsOptions = {
+  origin: "meetus.netlify.com",
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  allowedHeaders: "Content-Type",
+  optionsSuccessStatus: 200
+}
 
 const app = express();
 app.use(morgan('combined'));
@@ -23,4 +28,3 @@ socketioapp.socketIO(io);
 server.listen(9090, () => {
     console.log('Server running on port 9090');
 });
-
