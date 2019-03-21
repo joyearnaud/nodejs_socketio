eeac16d974c5869894138e3650eb0186cec15018
Arnaud J <joye.arnaud@gmail.com>
2019-03-21 14:43:43 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,16 +15,19 @@ const apiapp = require("./API/apiapp");
 //   optionsSuccessStatus: 200
 // }
 
+const PORT = process.env.PORT || 3000;
+
 const app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(cors());
 const server = http.createServer(app);
-const io = require('socket.io').listen(server);
+// const io = require('socket.io').listen(server);
+const io = require('socket.io')(server, {origins:'meetus.netlify.com:* https://meetus.netlify.com:* https://www.meetus.netlify.com:*'});
 
 apiapp.api(app);
 socketioapp.socketIO(io);
 
-server.listen(80, () => {
-    console.log('Server running on port 80');
+server.listen(PORT, () => {
+    console.log(`Listening on ${ PORT }`);
 });
