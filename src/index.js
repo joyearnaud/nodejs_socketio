21991ae5a5f4ecece83d039d75a2d1cdccfe1e8b
Arnaud J <joye.arnaud@gmail.com>
2019-03-21 14:17:31 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,17 +8,17 @@ const morgan = require('morgan');
 const socketioapp = require("./socketIO/socketioapp");
 const apiapp = require("./API/apiapp");
 
-const corsOptions = {
-  origin: "https://meetus.netlify.com",
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-  allowedHeaders: "Content-Type",
-  optionsSuccessStatus: 200
-}
+// const corsOptions = {
+//   origin: "https://meetus.netlify.com",
+//   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+//   allowedHeaders: "Content-Type",
+//   optionsSuccessStatus: 200
+// }
 
 const app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
-app.use(cors(corsOptions))
+app.use(cors());
 const server = http.createServer(app);
 const io = require('socket.io').listen(server);
 
