9e0c27cf67c8a99c08213760303ecca0a396281b
Arnaud J <joye.arnaud@gmail.com>
2019-03-23 23:16:20 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 app.use(cors());
 const server = http.createServer(app);
 // const io = require('socket.io').listen(server);
-const io = require('socket.io')(3000);
+const io = require('socket.io').listen(server);
 
 apiapp.api(app);
 socketioapp.socketIO(io);
